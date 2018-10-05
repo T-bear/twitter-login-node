23972d69cc704bffd5746591ade9cedaa8ec13cd
torbjorn.holgersson87@gmail.com <torbjorn.holgersson87@gmail.com>
2018-10-05 07:43:04 +0000

    first commit

---
+++ twitter-login-node/configuration/config.js
@@ -0,0 +1,15 @@
+module.exports={
+/*
+* This file contains the configurations information of Twitter login app.
+* It consists of Twitter app information, database information.
+*/
+
+	"twitter_api_key" 		: 			"ZPN1OESftezUvO87lvC5grqiw",
+	"twitter_api_secret"	:				"QQXKVLJzXegppb0T8WKmxXSldakS3KHPUoR5JdV2EE7GCxiLEp",
+	"callback_url"				:				"http://x00.se:1980/auth/twitter/callback",
+	"use_database"				:				"true",
+	"host"								:				"localhost",
+	"username"						:				"root",
+	"password"						:				"",
+	"database"						:				"demodb"
+}
