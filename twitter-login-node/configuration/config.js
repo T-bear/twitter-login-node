f272de116e181b335d87d6596276c7c025c9dc92
torbjorn.holgersson87@gmail.com <torbjorn.holgersson87@gmail.com>
2018-10-09 07:19:31 +0000

    uppdating backup

--- twitter-login-node/configuration/config.js
+++ twitter-login-node/configuration/config.js
@@ -6,6 +6,8 @@ module.exports={
     "facebook_api_key"      :     "274575856508098",
     "facebook_api_secret"   :     "b7884796b74cd2c2894d307ace28eeac",
 	"twitter_api_key" 		: 			"ZPN1OESftezUvO87lvC5grqiw",
+	"google_api_key"	:	"77296995766-m9ccuvf7k4hdmj3fg0hev9t0gctd281e.apps.googleusercontent.com",
+	"google_api_secret"	:	"FwgX12XOREVzqyJk3jjuSkx2",
 	"twitter_api_secret"	:				"QQXKVLJzXegppb0T8WKmxXSldakS3KHPUoR5JdV2EE7GCxiLEp",
 	"callback_urlt"				:				"http://x00.se:1980/auth/twitter/callback",
     "callback_urlf"				:				"http://x00.se:1980/auth/facebook/callback",
